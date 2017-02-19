c432b7821e3faf3f07d3a196df94d054c6c8445a
Caio Brentano <caio.passos@corp.globo.com>
2017-02-18 23:53:13 -0300

    Droping py3

--- setup.py
+++ setup.py
@@ -20,8 +20,6 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
     ],
     scripts=[],
     entry_points={
